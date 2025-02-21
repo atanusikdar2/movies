f60c9e86121aa5233839176042d7b7e098134e43
Moviestudio2006 <atanusikdar77@gmail.com>
2025-02-21 09:54:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -112,7 +112,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/moviestudio444">𝙊𝙧𝙞𝙜𝙞𝙣𝙖𝙡𝙨</a>」</b>"""
+    
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
