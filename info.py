fb7d15c40b12125d87590c91f029cbdf53b3275e
Moviestudio2006 <atanusikdar77@gmail.com>
2025-02-21 19:31:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://t.me/Atanu18") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002269301132'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK','https://t.me/moviebotstudio')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002396490225').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002260752244').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://atanusikdar77:Moviestudio@moviestudio.8d2sk.mongodb.net/?retryWrites=true&w=majority&appName=Moviestudio")
